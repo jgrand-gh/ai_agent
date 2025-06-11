40fa5598e50dfd316040aefe33b2327461c66081
faelim <jgrand@live.ca>
2025-06-11 16:21:28 -0500

    first draft of AI Agent for Boot.dev

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    model = "gemini-2.0-flash-001"
+    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    try:
+        response = client.models.generate_content(model=model, contents=contents)
+
+        print(f"{response.text}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    except Exception as e:
+        print("Something went wrong with the Gemini API call")
+        print(str(e))
+
+if __name__ == "__main__":
+    main()
