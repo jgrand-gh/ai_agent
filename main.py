9c98b1bdf46c7ba6ace81582b2349f5302bdf732
faelim <jgrand@live.ca>
2025-06-11 17:02:48 -0500

    added options for verbosity, as well as some error handling/scalability

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    model = "gemini-2.0-flash-001"
-    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    if len(sys.argv) > 1:
+        prompt = sys.argv[1]
+    else:
+        print("ERROR: A prompt must be provided as an argument")
+        sys.exit(1)
+
+    verbose = False
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "--verbose":
+            verbose = True
+
+    # temporary one-item list that will eventually hold the whole conversation
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)]),]
 
     try:
-        response = client.models.generate_content(model=model, contents=contents)
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages)
 
-        print(f"{response.text}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if not verbose:
+            print(f"{response.text}")
+        if verbose:
+            print(f"User prompt: {response.text}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     except Exception as e:
         print("Something went wrong with the Gemini API call")
         print(str(e))
