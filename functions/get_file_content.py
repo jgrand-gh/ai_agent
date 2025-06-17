8381eb2922d8df10761f76de0c0d32ff36611e44
faelim <jgrand@live.ca>
2025-06-17 13:36:12 -0500

    added get_file_content

---
+++ functions/get_file_content.py
@@ -0,0 +1,25 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    abs_w_dir = os.path.abspath(working_directory)
+    abs_path = os.path.abspath(os.path.join(abs_w_dir, file_path))
+
+    if not abs_path.startswith(abs_w_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    MAX_CHARS = 10000
+
+    try:
+        with open(abs_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if f.read(1):
+                file_content_string += f'\n[...File "{file_path}" truncated at 10000 characters]'
+    except Exception as e:
+        return f"Error reading file: {e}"
+
+    return file_content_string
+
+if __name__ == "__main__":
+    get_file_content("calculator", "main.py")
--- tests.py
+++ tests.py
@@ -1,21 +1,55 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-def main():
-    result = get_files_info("calculator", ".")
-    print("Testing for 'calculator' and '.':")
+def test_get_files_info():
+    function_name = "get_files_info"
+    first_arg = "calculator"
+    second_arg = ""
+
+    second_arg = "."
+    result = get_files_info(first_arg, second_arg)
+    print(f"Testing '{function_name}' for '{first_arg}' and '{second_arg}':")
     print(result + "\n")
 
-    result = get_files_info("calculator", "pkg")
-    print("Testing for 'calculator' and 'pkg':")
+    second_arg = "pkg"
+    result = get_files_info(first_arg, second_arg)
+    print(f"Testing '{function_name}' for '{first_arg}' and '{second_arg}':")
     print(result + "\n")
 
-    result = get_files_info("calculator", "/bin")
-    print("Testing for 'calculator' and '/bin':")
+    second_arg = "/bin"
+    result = get_files_info(first_arg, second_arg)
+    print(f"Testing '{function_name}' for '{first_arg}' and '{second_arg}':")
     print(result + "\n")
 
-    result = get_files_info("calculator", "../")
-    print("Testing for 'calculator' and '../':")
+    second_arg = "../"
+    result = get_files_info(first_arg, second_arg)
+    print(f"Testing '{function_name}' for '{first_arg}' and '{second_arg}':")
+    print(result + "\n")
+
+def test_get_file_content():
+    function_name = "get_file_content"
+    first_arg = "calculator"
+    second_arg = ""
+
+    second_arg = "main.py"
+    result = get_file_content(first_arg, second_arg)
+    print(f"Testing '{function_name}' for '{first_arg}' and '{second_arg}'")
+    print(result + "\n")
+
+    second_arg = "pkg/calculator.py"
+    result = get_file_content(first_arg, second_arg)
+    print(f"Testing '{function_name}' for '{first_arg}' and '{second_arg}'")
+    print(result + "\n")
+
+    second_arg = "/bin/cat"
+    result = get_file_content(first_arg, second_arg)
+    print(f"Testing '{function_name}' for '{first_arg}' and '{second_arg}'")
+    print(result + "\n")
+
+    second_arg = "lorem.txt"
+    result = get_file_content(first_arg, second_arg)
+    print(f"Testing '{function_name}' for '{first_arg}' and '{second_arg}'")
     print(result + "\n")
 
 if __name__ == "__main__":
-    main()
+    test_get_file_content()
