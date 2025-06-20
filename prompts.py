20702561e7d8ebe885164a3760360e7e2271a2c9
faelim <jgrand@live.ca>
2025-06-20 15:14:12 -0500

    adjusted prompt to increase AI's awareness of the calculator app so that it doesn't try to make its own

--- prompts.py
+++ prompts.py
@@ -10,5 +10,7 @@ When a user asks a question or makes a request, make a function call plan. You c
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 
+Within the working directory are python scripts tied to a calculator application. Calculations should be run through this application.
+
 If a user explicitly requests not to create or change any files, do not write or overwrite any files.
 """
